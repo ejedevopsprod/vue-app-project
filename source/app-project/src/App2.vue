5c34a042757956e85235806c2ff6067dad1bf77d
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-09 12:57:54 -0600

    update variables

---
+++ source/app-project/src/App2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <div>{{ variable1 }}</div>
+      <div>{{ variable2 }}</div>
+    </div>
+  </template>
+  <script>
+  import getEnv from '@/utils/env'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        variable1: getEnv('VUE_APP_VARIABLE_1'),
+        variable2: getEnv('VUE_APP_VARIABLE_2')
+      }
+    }
+  }
+  </script>
