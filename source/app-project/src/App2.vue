103c6b453e44b2f7a68d5c6da92547f7afd1c31b
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-09 14:43:29 -0600

    update App2.vue

--- source/app-project/src/App2.vue
+++ source/app-project/src/App2.vue
@@ -6,13 +6,12 @@
     </div>
   </template>
   <script>
-  import getEnv from '@/utils/env'
   export default {
     name: 'App',
     data() {
       return {
-        variable1: getEnv('VUE_APP_VARIABLE_1'),
-        variable2: getEnv('VUE_APP_VARIABLE_2')
+        variable1: process.env.VUE_APP_VARIABLE_1,
+        variable2: process.env.VUE_APP_VARIABLE_2,
       }
     }
   }
