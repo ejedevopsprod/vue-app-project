9efef76381efa9e4bd9cd510ed398f8158a92767
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 09:12:07 -0600

    update App3

--- source/app-project/src/App3.vue
+++ source/app-project/src/App3.vue
@@ -2,11 +2,7 @@
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
       <h1>{{ companyName }}</h1>
-      <p>{{ welcomeMessage }}</p>
-      <p><strong>Slogan:</strong> {{ tagline }}</p>
-      <p><strong>Email de Contacto:</strong> <a :href="'mailto:' + contactEmail">{{ contactEmail }}</a></p>
-      <p><strong>Teléfono:</strong> {{ phoneNumber }}</p>
-      <p><strong>Dirección:</strong> {{ address }}</p>
+      
       <div>Esta es la URL: {{ variable_url }}</div>
       <div>Esta es una variable: {{ variable_prueba }}</div>
     </div>
@@ -17,11 +13,6 @@
     data() {
       return {
         companyName: window.config.VUE_APP_COMPANY_NAME,
-        welcomeMessage: window.config.VUE_APP_WELCOME_MESSAGE,
-        tagline: window.config.VUE_APP_TAGLINE,
-        contactEmail: window.config.VUE_APP_CONTACT_EMAIL,
-        phoneNumber: window.config.VUE_APP_PHONE_NUMBER,
-        address: window.config.VUE_APP_ADDRESS,
         variable_url: window.config.VUE_APP_API_URL,
         variable_prueba: window.config.VUE_APP_CUSTOM_MESSAGE,
       }
