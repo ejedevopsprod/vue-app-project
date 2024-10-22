1edeba8fad75395caba7973d33760970e507c141
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 09:35:06 -0600

    update App

--- source/app-project/src/App.vue
+++ source/app-project/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-</template>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <div>Esta variable es : {{ variable_url }}</div>
+      <div>Esta variable es : {{ variable_message }}</div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        variable_url: window.config.VUE_APP_API_URL,
+        variable_message: window.config.VUE_APP_CUSTOM_MESSAGE,
+        variable_prod: window.config.VUE_APP_VARIABLE_PROD,
+      }
+    }
+  }
+  </script>
--- source/app-project/src/main.js
+++ source/app-project/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App1.vue'
+import App from './App.vue'
 
 
 // Cargar el archivo config.js
