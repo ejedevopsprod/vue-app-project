80ef131e922756e197355feb043688e7fb2f1f58
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 09:01:48 -0600

    update main

--- source/app-project/src/main.js
+++ source/app-project/src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import App from './App1.vue'
+import App from './App3.vue'
 
+
+// Cargar el archivo config.js
+fetch('/config.js')
+  .then((response) => response.text())
+  .then((configScript) => {
+    // Ejecutar el script config.js para acceder a la variable global window.config
+    const script = document.createElement('script');
+    script.text = configScript;
+    document.head.appendChild(script);
 
   createApp(App).mount('#app')
+
+  .catch((error) => {
+    console.error('Error al cargar config.js:', error);
+  });
+
+    //createApp(App).mount('#app')
+  })
