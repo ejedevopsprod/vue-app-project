ad508ab529e2b7180f56e533549e8c902e60256c
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 09:22:19 -0600

    .

--- source/app-project/src/App1.vue
+++ source/app-project/src/App1.vue
@@ -3,16 +3,6 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
