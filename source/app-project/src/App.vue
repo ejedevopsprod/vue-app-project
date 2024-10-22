62d16c1850da2dbd1118bb4d6fc4923eca9afc12
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-21 18:02:10 -0600

    update app.vue

--- source/app-project/src/App.vue
+++ source/app-project/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-
-<template>
   <div id="app">
     <WelcomeMessage />
   </div>
