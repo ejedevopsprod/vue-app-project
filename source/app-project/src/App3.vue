e2480b87830f9ebd72675672bfcf63d7558de20e
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 10:07:00 -0600

    .

--- source/app-project/src/App3.vue
+++ source/app-project/src/App3.vue
@@ -1,24 +1,10 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
-      <h1>{{ companyName }}</h1>
-      
-      <div>Esta es la URL: {{ variable_url }}</div>
-      <div>Esta es una variable: {{ variable_prueba }}</div>
     </div>
   </template>
-  <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        companyName: window.config.VUE_APP_COMPANY_NAME,
-        variable_url: window.config.VUE_APP_API_URL,
-        variable_prueba: window.config.VUE_APP_CUSTOM_MESSAGE,
-      }
-    }
-  }
-  </script>
+
+
 
 <style>
 #app {
