1e156473b09261267f621a4144542035d3422277
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 14:57:00 -0600

    Update App3.vue

--- source/app-project/src/App3.vue
+++ source/app-project/src/App3.vue
@@ -1,18 +1,34 @@
 <template>
-    <div id="app">
-      <img alt="Vue logo" src="./assets/logo.png">
-      <div>Esta es la URL: {{ variable_url }}</div>
-      <div>Esta es una variable: {{ variable_prueba }}</div>
+  <div id="app">
+    <div class="welcome">
+      <h1>{{ companyName }}</h1>
+      <p>{{ welcomeMessage }}</p>
+      <p><strong>Slogan:</strong> {{ tagline }}</p>
+      <p><strong>Email de Contacto:</strong> <a :href="'mailto:' + contactEmail">{{ contactEmail }}</a></p>
+      <p><strong>Teléfono:</strong> {{ phoneNumber }}</p>
+      <p><strong>Dirección:</strong> {{ address }}</p>
     </div>
-  </template>
-  <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        variable_url: window.config.VUE_APP_API_URL,
-        variable_prueba: window.config.VUE_APP_CUSTOM_MESSAGE,
-      }
-    }
-  }
-  </script>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      companyName: window.config.VUE_APP_COMPANY_NAME,
+      welcomeMessage: window.config.VUE_APP_WELCOME_MESSAGE,
+      tagline: window.config.VUE_APP_TAGLINE,
+      contactEmail: window.config.VUE_APP_CONTACT_EMAIL,
+      phoneNumber: window.config.VUE_APP_PHONE_NUMBER,
+      address: window.config.VUE_APP_ADDRESS,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#welcome {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
