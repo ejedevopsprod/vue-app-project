103e81879b3df1a2b562abc35290115f0e541b74
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 10:12:05 -0600

    update

--- source/app-project/src/App.vue
+++ source/app-project/src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div id="app">
-    <div class="welcome">
-      <h1>{{ companyName }}</h1>
-      <p>{{ welcomeMessage }}</p>
-      <p><strong>Slogan:</strong> {{ tagline }}</p>
-      <p><strong>Email de Contacto:</strong> <a :href="'mailto:' + contactEmail">{{ contactEmail }}</a></p>
-      <p><strong>Teléfono:</strong> {{ phoneNumber }}</p>
-      <p><strong>Dirección:</strong> {{ address }}</p>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-export default {
-  data() {
-    return {
-      companyName: window.config.VUE_APP_COMPANY_NAME,
-      welcomeMessage: window.config.VUE_APP_WELCOME_MESSAGE,
-      tagline: window.config.VUE_APP_TAGLINE,
-      contactEmail: window.config.VUE_APP_CONTACT_EMAIL,
-      phoneNumber: window.config.VUE_APP_PHONE_NUMBER,
-      address: window.config.VUE_APP_ADDRESS,
-    };
-  },
-};
-</script>
 
-<style scoped>
-#welcome {
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 50px;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ source/app-project/src/App1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="welcome">
+      <h1>{{ companyName }}</h1>
+      <p>{{ welcomeMessage }}</p>
+      <p><strong>Slogan:</strong> {{ tagline }}</p>
+      <p><strong>Email de Contacto:</strong> <a :href="'mailto:' + contactEmail">{{ contactEmail }}</a></p>
+      <p><strong>Teléfono:</strong> {{ phoneNumber }}</p>
+      <p><strong>Dirección:</strong> {{ address }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      companyName: window.config.VUE_APP_COMPANY_NAME,
+      welcomeMessage: window.config.VUE_APP_WELCOME_MESSAGE,
+      tagline: window.config.VUE_APP_TAGLINE,
+      contactEmail: window.config.VUE_APP_CONTACT_EMAIL,
+      phoneNumber: window.config.VUE_APP_PHONE_NUMBER,
+      address: window.config.VUE_APP_ADDRESS,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#welcome {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
