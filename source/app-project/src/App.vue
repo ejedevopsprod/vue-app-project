f1b204211f65c349fbdff9f521265cf1231f2c34
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 15:33:10 -0600

    update App

--- source/app-project/src/App.vue
+++ source/app-project/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
+      <h1>{{ companyName }}</h1>
       <div>Esta es la URL: {{ variable_url }}</div>
       <div>Esta es una variable: {{ variable_prueba }}</div>
     </div>
@@ -11,6 +12,7 @@
     name: 'App',
     data() {
       return {
+        companyName: process.env.VUE_APP_COMPANY_NAME,
         variable_url: window.config.VUE_APP_API_URL,
         variable_prueba: window.config.VUE_APP_CUSTOM_MESSAGE,
       }
