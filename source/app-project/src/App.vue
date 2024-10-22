878436a5b1ce55a4db36c55e2663b428ab0144d4
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-22 12:23:41 -0600

    .

--- source/app-project/src/App.vue
+++ source/app-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
-      <div> Company name: {{companyName}}</div>
+      <div> Company name: </div>
     </div> 
   </template>
 
@@ -17,15 +17,3 @@
     }
   }
   </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
